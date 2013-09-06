9d309ddd8fd48ab066c401aa1482c2f43fadec2c
administrateur <olivier_duhamel@hotmail.com>
2013-09-06 16:07:39 -0400

    header

---
+++ login.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: administrateur
+ * Date: 13-09-06
+ * Time: 14:08
+ * To change this template use File | Settings | File Templates.
+ */echo "login:";
+echo "password:";?>
+
+<form method="post" action="login.php">
+
+    <p>login : <input type="text" name="login" /></p>
+    <p>password : <input type="text" name="password" /></p>
+
+
+      <Input type = 'Radio' Name ='gender' value= 'male'
+            <?PHP print $male_status; ?>
+            >Male
+
+      <Input type = 'Radio' Name ='gender' value= 'female'
+            <?PHP print $female_status; ?>
+            >Female
+
+     <P>
+    <p><input type="submit" name="valider" value="OK"></p>
+    </form>
+<?php
+    if(isset($_POST['valider']))
+    {
+        if(isset($_POST['login']) && $_POST['login'] === "Olivier")
+        {
+            $expire = 365*24*3600;
+            setcookie("login","gender",time()+$expire);
+            header("Location: secret.php");
+            //setcookie("cookie[login]", "login");
+            //setcookie("cookie[gender]", "gender");
+            //setcookie("cookie[password]", "password");
+        }
+    }
+?>
--- secret.php
+++ secret.php
@@ -3,13 +3,7 @@
  * Created by JetBrains PhpStorm.
  * User: administrateur
  * Date: 13-09-06
- * Time: 13:05
+ * Time: 14:08
  * To change this template use File | Settings | File Templates.
  */
-echo "<h1>Gros-Lourd</h1></br>";?>
-<a href="MOI.html">MOI     </a>
-<a href="login.php">login    </a>
-<a href="xd.html">XD     </a>
-
-
-
+echo "allo";
